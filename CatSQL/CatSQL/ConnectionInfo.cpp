65318efe44c1302df19d25bddc4896d2d38ec75b
jsbrown1986 <46855858+jsbrown1986@users.noreply.github.com>
2019-03-01 19:21:08 -0800

    Again, 'nother tweak with comments.

--- CatSQL/CatSQL/ConnectionInfo.cpp
+++ CatSQL/CatSQL/ConnectionInfo.cpp
@@ -1,11 +1,11 @@
-/************************************************************************************
-*																					                                          *
-*		CatSQL by Jacob Brown (3/1/2019) - Written in C/C++							                *
-*																					                                          *
-*		ConnectionInfo.cpp - An object that is passed to ServerHandler.cpp			        *
-*		that carries all the relevant info to attempt a server connection 			        *
-*																					                                          *
-************************************************************************************/
+/************************************************************************************************
+*												*
+*		CatSQL by Jacob Brown (3/1/2019) - Written in C/C++				*
+*												*
+*		ConnectionInfo.cpp - An object that is passed to ServerHandler.cpp		*
+*		that carries all the relevant info to attempt a server connection.		*
+*												*
+************************************************************************************************/
 #include "ConnectionInfo.h"
 
 // Constructor
