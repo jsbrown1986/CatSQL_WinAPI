e73f4dc6e3e47945a9187201b7260bdc8644c4b3
jsbrown1986 <46855858+jsbrown1986@users.noreply.github.com>
2019-03-01 19:14:55 -0800

    Tweaked the formatting of the comments at top.

--- CatSQL/CatSQL/ConnectionInfo.cpp
+++ CatSQL/CatSQL/ConnectionInfo.cpp
@@ -1,10 +1,10 @@
 /************************************************************************************
-*																					*
-*		CatSQL by Jacob Brown (3/1/2019) - Written in C/C++							*
-*																					*
-*		ConnectionInfo.cpp - An object that is passed to ServerHandler.cpp			*
-*		that carries all the relevant info to attempt a server connection.			*
-*																					*
+*																					                                          *
+*		CatSQL by Jacob Brown (3/1/2019) - Written in C/C++							                *
+*																					                                          *
+*		ConnectionInfo.cpp - An object that is passed to ServerHandler.cpp			        *
+*		that carries all the relevant info to attempt a server connection.			        *
+*																					                                          *
 ************************************************************************************/
 #include "ConnectionInfo.h"
 
